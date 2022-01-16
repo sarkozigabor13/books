af9e2bb58d437e5b0d2ef25fc85493a8a3334758
Sárközi Gábor <sarkozigabor13@gmail.com>
2022-01-16 21:58:17 +0100

    Filter tab created, books price added

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,22 +21,35 @@
     })
 }
 
+// Fetch by search input 
+let searchFetch = (e) => {
+
+    e.preventDefault();
+    const searchInput = e.target.elements.searchInput.value;
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=AIzaSyAkvnYin1tdFYA6D5PfQpNJpnj8yI0xjDM`)
+        .then(response => response.json())
+        .then(data => {
+
+            // Call render function
+            renderUI(data);
+        })
+}
 
 // Send search form 
 {
     const searchForm = document.querySelector('.search-form');
+    searchForm.addEventListener('submit', searchFetch)
+}
 
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const searchInput = e.target.elements.searchInput.value;
+// Apply filters 
+{
+    let applyBtn = document.querySelector('.btn-filter-apply');
+    applyBtn.addEventListener('click', () => {
 
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=AIzaSyAkvnYin1tdFYA6D5PfQpNJpnj8yI0xjDM`)
-            .then(response => response.json())
-            .then(data => {
-
-                // Call render function
-                renderUI(data);
-            })
+        let filterFree = document.querySelector('#filterPriceFree');
+        let filterPaid = document.querySelector('#filterPricePaid');
+        console.log(filterFree.checked, filterPaid.checked);
     })
 }
 
@@ -63,17 +76,19 @@ function renderUI(data) {
 
         let bookTitle = book.volumeInfo.title;
         let bookAuthor = book.volumeInfo.authors;
-        let bookCategory = book.volumeInfo.categories === undefined ? "Ismeretlen" : book.volumeInfo.categories;book.volumeInfo.categories;
-        let smallThumbnail = book.volumeInfo.readingModes.image === true ? book.volumeInfo.imageLinks.smallThumbnail : "./assets/img/missing_photo.png";       
+        let bookCategory = book.volumeInfo.categories === undefined ? "Ismeretlen" : book.volumeInfo.categories; book.volumeInfo.categories;
+        let smallThumbnail = book.volumeInfo.readingModes.image === true ? book.volumeInfo.imageLinks.smallThumbnail : "./assets/img/missing_photo.png";
+        let bookPrice = book.saleInfo.saleability === "NOT_FOR_SALE" ? "Nem eladó" : book.saleInfo.listPrice.amount + 'HUF';
 
         console.log(bookCategory);
-         bookCardHtml += `
+        bookCardHtml += `
         <div class="col-md-6 d-block search-result__col">
             <div class="row">
                 <div class="col-8">
                     <p class="mb-1">${bookTitle}</p>
                     <p class="mb-1"><small class="text-muted">Author: ${bookAuthor}</small></p>
                     <p class="mb-1"><small class="text-muted">Category: ${bookCategory}</small></p>
+                    <p class="mb-1"><small class="text-muted">Price: ${bookPrice}</small></p>
                     <div class="search-result__action">
                         <button class="btn btn-primary">Bővebb információ</button>
                     </div>
